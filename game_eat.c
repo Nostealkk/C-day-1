ad3f447ee540a1936a5034dd5dab5719a9d717f9
Nostealkk <120027253+Nostealkk@users.noreply.github.com>
2023-01-16 20:47:40 +0800

    1月16日-贪吃蛇制作

--- game_eat.c
+++ game_eat.c
@@ -6,102 +6,160 @@
 #include <string.h>
 #include <stdlib.h>
 
+//地图的大小
+//#define xlength 50
+//#define ylength 20
+//
 
-void boundary();
+//用一个结构定义一个二维数组用来存放内容
+//typedef struct _map {
+//	char map[ylength][xlength];
+//}Map;
 
-void start();
+//定义蛇的身体是怎么组成的
+//typedef struct _body {
+//	char a;
+//	struct _body* next;
+//}Body;
 
+//typedef struct _body {
+//	struct _bodypc *head;
+//	struct _bodypc *tail;
+//}Body;
+
+
+//负责在数组里面制造边界
+void boundary(Map *pmap);
+
+//负责在数组里面输入界面
+void startpage(Map *pmap);
+
+//负责游戏整体运行
 void game();
 
-void map();
-
+//负责食物的出现和消失
 void food();
 
+//负责蛇的出现和消失以及移动设定
 void snake();
 
+//设置死亡条件
 void died();
+
+//负责蛇的移动部分
+void move();
+
+//负责将每次数组的内容输出
+void print(Map* pmap);
+
+int control();
 
 
 int main(int argc, char const argv[])
 {
-	//printf("%d\n", 0xCC);
-	//printf("%d\n", 0xB0);
+	Map map;
+	//初试化阶段
+	boundary(&map);
+	food(&map);
+	snake(&map);
 
-	//printf("%d\n", 0xB3);
-	//printf("%d\n", 0xD4);
-
-	//printf("%d\n", 0xC9);
-	//printf("%d\n", 0xDF);
-
-	printf("汉");
-	boundary();
+	//操控阶段
 
 
+
+	//输出结果阶段
+	print(&map);
+	
+	
 	return 0;
 }
 
-void boundary()
+void boundary(Map *pmap)
 {
+	system("cls");
 	int x, y;
-	//char* k;
-	char p[20][50] = {"0"};
-	//k = (char*)malloc(sizeof(char)*20*50);
-
-	for (y = 0;y<20;y++) {
-		for (x = 0;x < 50;x++) {
-			if (y==0||y==19) {
-				p[y][x] = '5';
-			} else if (0<y<19){
-				if (x == 0 || x == 48) {
-					p[y][x] = '1';
+	for (y = 0;y<ylength;y++) {
+		for (x = 0;x < xlength;x++) {
+			if (y==0||y==ylength-1) {
+				pmap->map[y][x] =1;
+			} else if (0<y<ylength-1){
+				if (x == 0 || x == xlength-2) {
+					pmap->map[y][x] =1;
 				}
-				else if (0<x<49){
-					p[y][x] = ' ';
+				else if (0<x<xlength-1){
+					pmap->map[y][x] =0;
 				}
 			}
 	  }
 	}
-
-	p[5][24] = 186;
-	p[5][26] = 186;
-
-
-	for (y=0;y < 20;y++) {
-		for (x = 0;x < 50;x++) {
-			if (x<49) {
-				printf("%c", p[y][x]);
-			}
-			else {
-				printf("\n");
-			}
-
-		}
-	}
-
-	//free(p);
-	//char* p = NULL;
 }
 
-void start()
+//void startpage(Map *pmap)
+//{
+//	int a, b;
+//
+//	
+//	pmap->map[5][17] = 204;
+//	pmap->map[5][18] = 176;
+//
+//	pmap->map[5][20] = 179;
+//	pmap->map[5][21] = 212;
+//
+//	pmap->map[5][23] = 201;
+//	pmap->map[5][24] = 223;
+//
+//
+//
+//}
+
+void print(Map *pmap)
 {
-	printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
-	printf("$                                                                $\n");
-	printf("$                                                                $\n");
-	printf("$                                                                $\n");
-	printf("$                           贪吃蛇                                $\n");
-	printf("$                                                                $\n");
-	printf("$                                                                $\n");
-	printf("$                                                                $\n");
-	printf("$                                                                $\n");
-	printf("$               按 1 进入游戏       按 0 退出游戏                    $\n");
-	printf("$                                                                $\n");
-	printf("$                                                                $\n");
-	printf("$                                                                $\n");
-	printf("$                                                                $\n"); 
-	printf("$                  made by williamkk                             $\n");
-	printf("$                                                                $\n");
-	printf("$                                                                $\n");
-	printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+	int y, x;
+	for (y = 0;y < ylength;y++) {
+		for (x = 0;x < xlength;x++) {
+			if (x == xlength - 1) {
+				printf("\n");
+			}else if (pmap->map[y][x] == 1) {//这是边界
+				printf("*");
+			}else if (pmap->map[y][x] == 0) {//这是空地
+				printf(" ");
+			}else if (pmap->map[y][x] == -1) {//这是食物
+				printf("★");
+			}else if (pmap->map[y][x] == 2) {//这是蛇的头
+				printf("@");
+			}else if (pmap->map[y][x] >2) {//这是蛇的身子
+				printf("^");
+			}
+		}
+
+	}
+}
+
+void food(Map *pmap)
+{
+	unsigned int i, j;
+	srand((unsigned int)time(NULL));
+    i = rand() % (ylength-2)+1;
+	j = rand() % (xlength-3)+1;
+    if (pmap->map[i][j] == 0) 
+	{
+		pmap->map[i][j] = -1;
+	}
+	printf("%d\n", i);
+	printf("%d\n", j);
+}
+
+void snake(Map*pmap)
+{
+	//初始化，先让蛇出现在棋盘之上
+	pmap->map[5][10] = 2;
+	pmap->map[5][11] = 3;
+	pmap->map[5][12] = 3;
+
 
 }
 
+int control() {
+
+
+}
--- game_eat.h
+++ game_eat.h
@@ -3,9 +3,23 @@
 
 #ifndef _GAME_EAT_
 #define _GAME_EAT_
+#define xlength 50
+#define ylength 20
 
 
+typedef struct _map {
+	int map[ylength][xlength];
+}Map;
 
+typedef struct _bodypc {
+	int a;//这个值是为了输出在数组里面的值
+	struct _map *loc;//在二维数组里面的位置
+	struct _body *next;//链表形成蛇的身体
+}Bodypc;
 
+typedef struct _body {
+	struct _bodypc *head;//指定蛇的头部
+	struct _bodypc *tail;//指定蛇的尾巴
+}Body;
 
 #endif // !_GAME_EAT_
